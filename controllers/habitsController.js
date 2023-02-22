5b9d9de1c35aad40aea3752ab826e5ea9b081afd
Vishal Kumar <amvishal09@outlook.com>
2023-02-22 19:04:34 +0530

    added comment

--- controllers/habitsController.js
+++ controllers/habitsController.js
@@ -26,16 +26,17 @@ module.exports.fetchWeeklyData = (req, res) => {
   });
 };
 
-// Updates the status of a habit
+// Export a function to update the status of a habit for a specific day
 module.exports.updateHabitStatus = (req, res) => {
-  const habitId = req.params.id;
-  const day = req.params.day;
-  const status = req.params.status;
-  HabitModel.findById(habitId, (error, habit) => {
+  // Retrieve the habit ID, day, and status from the request object
+  const { id, day, status } = req.params;
+  // Find the habit with the specified ID
+  HabitModel.findById(id, (error, habit) => {
     if (error) {
       console.log(error);
       return res.redirect("back");
     }
+    // Update the status of the habit for the specified day and save the changes
     habit.days[day] = status;
     habit.save();
     updateStreakAndCompleted(habit);
@@ -43,28 +44,35 @@ module.exports.updateHabitStatus = (req, res) => {
   });
 };
 
-// Updates the streak and completed values for a habit
+// Helper function to update the streak and completed values for a habit
 const updateStreakAndCompleted = async (habit) => {
   try {
     let currentCompletedCount = 0;
     let maxStreakCount = 0;
     let currentStreakCount = 0;
+    // Iterate over the days of the habit
     for (let i = 0; i < habit.days.length; i++) {
       if (habit.days[i] == "Done") {
+        // If the day is completed, increment the completed count and current streak count
         currentCompletedCount++;
         currentStreakCount++;
       } else {
+        // If the day is not completed, check if the current streak count is greater than the maximum streak count
         if (currentStreakCount > maxStreakCount) {
+          // If it is, update the maximum streak count and reset the current streak count
           maxStreakCount = currentStreakCount;
           currentStreakCount = 0;
         } else {
+          // Otherwise, just reset the current streak count
           currentStreakCount = 0;
         }
       }
     }
+    // If the current streak count is greater than the max streak count, update the max streak count
     if (currentStreakCount > maxStreakCount) {
       maxStreakCount = currentStreakCount;
     }
+    // Update the habit document in the database with the new streak and completed values
     await HabitModel.findByIdAndUpdate(habit.id, {
       streak: maxStreakCount,
       completed: currentCompletedCount,
